83cbc5ce6e43e60d42d342cb0c670d023696841a
Pedro <pedro.borges@estudante.ufscar.br>
2024-04-08 19:42:02 -0300

    testes

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    retorno = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        retorno += string[i];
+    }
+    return retorno;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if ((a < 0) || (b < 0)) return "ERROR";
 
+    let soma = 0;
+    if(a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (a; a <= b; a++) {
+        soma += a;
+    }
+
+    return soma;
 };
 
 // Do not edit below this line
